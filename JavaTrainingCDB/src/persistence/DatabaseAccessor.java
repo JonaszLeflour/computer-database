c984fe0bc456250816f573dfad2bc627fd00e33b
root <root@excilys-Precision-WorkStation-T3500>
2018-10-17 15:58:13 +0200

    ui : added list computers/companies + show computer

--- JavaTrainingCDB/src/persistence/DatabaseAccessor.java
+++ JavaTrainingCDB/src/persistence/DatabaseAccessor.java
@@ -1,4 +1,5 @@
 package persistence;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -6,124 +7,111 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Map;
 
-
-
-
-
 public class DatabaseAccessor {
 	private String URL = "jdbc:mysql://localhost:3306/computer-database-db"
-    		+"?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=CET&useSSL=false";
-    private String user = "admincdb";
-    private String password = "qwerty1234";
-	
-    private Connection con = null;
-    
-    public static enum ComputerField{
-    	name,
-    	introduced,
-    	discontinued,
-    	company_id
-    }
-    
-    public static enum CompanyField{
-    	name
-    }
-	
-	public DatabaseAccessor(){
+			+ "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=CET&useSSL=false";
+	private String user = "admincdb";
+	private String password = "qwerty1234";
+
+	private Connection con = null;
+
+	public static enum ComputerField {
+		name, introduced, discontinued, company_id
+	}
+
+	public static enum CompanyField {
+		name
+	}
+
+	public DatabaseAccessor() {
 		try {
-			reconnect(user,password);
+			reconnect(user, password);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
 
 	public void reconnect(String user, String password) throws SQLException {
-		if(con != null) {
+		if (con != null) {
 			con.close();
 		}
 		con = DriverManager.getConnection(URL, user, password);
 		this.user = user;
-		this.password = password; 
+		this.password = password;
 	}
-	
+
 	public ResultSet executeQuery(String SQL) {
-		try{
-        	Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery(SQL);
-            return rs;
-        }
-        // Handle any errors that may have occurred.
-        catch (SQLException e) {
-            e.printStackTrace();
-            return null;
-        }
+		try {
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(SQL);
+			return rs;
+		}
+		// Handle any errors that may have occurred.
+		catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
-	
+
 	public void executeUpdate(String SQL) {
-		try{
-        	Statement stmt = con.createStatement();
-        	stmt.executeUpdate(SQL);
-        }
-        // Handle any errors that may have occurred.
-        catch (SQLException e) {
-            e.printStackTrace();
-        }
+		try {
+			Statement stmt = con.createStatement();
+			stmt.executeUpdate(SQL);
+		}
+		// Handle any errors that may have occurred.
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
-	
-	public ResultSet getAllComputers(){
-        return executeQuery("SELECT * FROM computer");
-    }
-	
-	public ResultSet getAllCompanies(){
-        return executeQuery("SELECT * FROM company");
-    }
-	
-	public ResultSet getCompanybyId(int id){
-        return executeQuery("SELECT * FROM company WHERE id="+id);
-    }
-	
+
+	public ResultSet getAllComputers() {
+		return executeQuery("SELECT * FROM computer");
+	}
+
+	public ResultSet getAllCompanies() {
+		return executeQuery("SELECT * FROM company");
+	}
+
+	public ResultSet getCompanybyId(int id) {
+		return executeQuery("SELECT * FROM company WHERE id=" + id);
+	}
+
 	public ResultSet getComputerById(int id) {
-		return executeQuery("SELECT * FROM computer WHERE id="+id);
+		return executeQuery("SELECT * FROM computer WHERE id=" + id);
 	}
-	
+
 	public ResultSet getComputerByName(String name) {
-		return executeQuery("SELECT * FROM computer WHERE name='"+name+"'");
+		return executeQuery("SELECT * FROM computer WHERE name='" + name + "'");
 	}
-	
-	public void createComputer(Map<ComputerField,String>columns) {
+
+	public void createComputer(Map<ComputerField, String> columns) {
 		StringBuffer requestBuf = new StringBuffer();
 		StringBuffer requestBuf_p2 = new StringBuffer();
 		requestBuf.append("INSERT INTO computer ");
 		requestBuf_p2.append(" VALUES ");
-		
+
 		boolean firstField = true;
-		for(Map.Entry<ComputerField, String> entry : columns.entrySet()){
-			if(!firstField) {
+		for (Map.Entry<ComputerField, String> entry : columns.entrySet()) {
+			if (!firstField) {
 				requestBuf.append(",");
 				requestBuf_p2.append(",");
-			}
-			else {
+			} else {
 				requestBuf.append("(");
 				requestBuf_p2.append("(");
 			}
 			requestBuf.append(entry.getKey());
-			//cast datetime if field is date like so : CAST('YYYY-MM-DD' AS DATETIME)
-			if(entry.getKey().equals(ComputerField.introduced) || entry.getKey().equals(ComputerField.discontinued)) {
+			// cast datetime if field is date like so : CAST('YYYY-MM-DD' AS DATETIME)
+			if (entry.getKey().equals(ComputerField.introduced) || entry.getKey().equals(ComputerField.discontinued)) {
 				requestBuf_p2.append("CAST('").append(entry.getValue()).append("' AS DATETIME)");
-			}
-			else if(entry.getKey().equals(ComputerField.name)) {
+			} else if (entry.getKey().equals(ComputerField.name)) {
 				requestBuf_p2.append("'").append(entry.getValue()).append("'");
-			}
-			else {
+			} else {
 				requestBuf_p2.append(entry.getValue());
 			}
-			
-			
+
 			firstField = false;
 		}
-		if(!firstField) {
+		if (!firstField) {
 			requestBuf.append(")");
 			requestBuf_p2.append(")");
 		}
@@ -131,37 +119,34 @@ public class DatabaseAccessor {
 		System.out.println(requestBuf.toString());
 		executeUpdate(requestBuf.toString());
 	}
-	
+
 	public void deleteComputerByName(String name) {
-		executeUpdate("DELETE FROM computer WHERE name = '"+name+"'");
+		executeUpdate("DELETE FROM computer WHERE name = '" + name + "'");
 	}
-	
-	public void updateComputerById(int id, Map<ComputerField,String>updatedColumns) {
+
+	public void updateComputerById(int id, Map<ComputerField, String> updatedColumns) {
 		StringBuffer request = new StringBuffer();
 		request.append("UPDATE computer SET ");
-		
+
 		boolean firstField = true;
-		for(Map.Entry<ComputerField, String> entry : updatedColumns.entrySet()){
-			if(!firstField) {
+		for (Map.Entry<ComputerField, String> entry : updatedColumns.entrySet()) {
+			if (!firstField) {
 				request.append(", ");
 			}
 			request.append(entry.getKey()).append("=");
-			
-			//cast datetime if field is date like so : CAST('YYYY-MM-DD' AS DATETIME)
-			if(entry.getKey().equals(ComputerField.introduced) || entry.getKey().equals(ComputerField.discontinued)) {
+
+			// cast datetime if field is date like so : CAST('YYYY-MM-DD' AS DATETIME)
+			if (entry.getKey().equals(ComputerField.introduced) || entry.getKey().equals(ComputerField.discontinued)) {
 				request.append("CAST('").append(entry.getValue()).append("' AS DATETIME)");
-			}
-			else if(entry.getKey().equals(ComputerField.name)) {
+			} else if (entry.getKey().equals(ComputerField.name)) {
 				request.append("'").append(entry.getValue()).append("'");
-			}
-			else {
+			} else {
 				request.append(entry.getValue());
 			}
 			firstField = false;
 		}
-		request.append(" WHERE id = "+id);
+		request.append(" WHERE id = " + id);
 		executeUpdate(request.toString());
 	}
-	
-	
+
 }
--- JavaTrainingCDB/src/ui/ConsoleUserInterface.java
+++ JavaTrainingCDB/src/ui/ConsoleUserInterface.java
@@ -1,60 +1,299 @@
 package ui;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
-import java.io.*;
+
+import service.DataPresenter;
+import model.Company;
+import model.Computer;
+import persistence.DatabaseAccessor.ComputerField;
 
 public class ConsoleUserInterface implements UserInterface {
 	Scanner scan = new Scanner(System.in);
-	public ConsoleUserInterface(){
-		
+	DataPresenter dp = new DataPresenter();
+
+	public ConsoleUserInterface() {
+
 	}
 
 	@Override
 	public void mainLoop(String[] args) {
 		boolean looping = true;
-		
-		while(looping) {
+
+		while (looping) {
 			System.out.println("Select option :");
 			System.out.println("1 - List computers");
 			System.out.println("2 - List companies");
 			System.out.println("3 - Show computer");
 			System.out.println("4 - Update computer infos");
-			System.out.println("5 - Delete computer");
+			System.out.println("5 - Add computer");
+			System.out.println("6 - Delete computer");
 			System.out.println("0 - Exit");
-			
-			int choice = scan.nextInt();
-			switch(choice) {
-				case 1: listComputers(); break;
-				case 2: listCompanies();break;
-				case 3: showComputer();break;
-				case 4: updateComputer();break;
-				case 5: deleteComputer(); break;
-				case 0: looping = false;break;
-				default : System.out.println("Invalid option");break;
+
+			String choice = scan.nextLine();
+			switch (choice) {
+			case "1":
+				listComputers();
+				break;
+			case "2":
+				listCompanies();
+				break;
+			case "3":
+				showComputer();
+				break;
+			case "4":
+				updateComputer();
+				break;
+			case "5":
+				addComputer();
+				break;
+			case "6":
+				deleteComputer();
+				break;
+			case "0":
+				looping = false;
+				break;
+			default:
+				System.out.println("Invalid option");
+				break;
 			}
-			
+
 		}
 		System.out.println("Bye");
 	}
 
 	private void listComputers() {
-		System.out.println("List computers");
+		List<Computer> computers = dp.getComputers();
+		int pageSize = 10;
+		int currentPage = 0;
+		int nbPages = (int) Math.ceil(computers.size() / (double) pageSize);
+		while (true) {
+			int start = currentPage * pageSize;
+			int end = Math.min((currentPage + 1) * pageSize, computers.size());
+			List<Computer> page = computers.subList(start, end);
+			for (Computer computer : page) {
+				System.out.println(computer.getId() + " - " + computer.getName());
+			}
+			System.out.println("Page " + (currentPage + 1) + " / " + nbPages);
+
+			// 4 cases : full list displayed, start of list displayed, end of list
+			// displayed, middle list displayed
+			// full display
+			boolean validChoice = false;
+			if (nbPages == 1) {
+				System.out.println("e to exit");
+				while (!validChoice) {
+					String choice = scan.nextLine();
+					if (!choice.equals("e")) {
+						System.out.println("Invalid choice");
+					} else {
+						validChoice = true;
+						return;
+					}
+				}
+
+			} else {
+				// middle list
+				if (currentPage != 0 && currentPage < (nbPages - 1)) {
+					System.out.println("n for next page - p for previous page - e to exit");
+					while (!validChoice) {
+						String choice = scan.nextLine();
+						switch (choice) {
+						case "n":
+							currentPage++;
+							validChoice = true;
+							break;
+						case "p":
+							currentPage--;
+							validChoice = true;
+							break;
+						case "e":
+							return;
+						default:
+							System.out.println("Invalid choice");
+							break;
+
+						}
+					}
+				}
+				// start list
+				else if (currentPage == 0) {
+					System.out.println("n for next page - e to exit");
+					while (!validChoice) {
+						String choice = scan.nextLine();
+						switch (choice) {
+						case "n":
+							currentPage++;
+							validChoice = true;
+							break;
+						case "e":
+							return;
+						default:
+							System.out.println("Invalid choice");
+							break;
+
+						}
+					}
+				}
+				// end list
+				else if (currentPage == nbPages - 1) {
+					System.out.println("p for previous page - e to exit");
+					while (!validChoice) {
+						String choice = scan.nextLine();
+						switch (choice) {
+						case "p":
+							currentPage--;
+							validChoice = true;
+							break;
+						case "e":
+							return;
+						default:
+							System.out.println("Invalid choice");
+							break;
+
+						}
+					}
+				}
+
+			}
+		}
 	}
-		
+
 	private void listCompanies() {
-		System.out.println("List companies");
+		List<Company> companies = dp.getCompanies();
+		int pageSize = 10;
+		int currentPage = 0;
+		int nbPages = (int) Math.ceil(companies.size() / (double) pageSize);
+		while (true) {
+			int start = currentPage * pageSize;
+			int end = Math.min((currentPage + 1) * pageSize, companies.size());
+			List<Company> page = companies.subList(start, end);
+			for (Company computer : page) {
+				System.out.println(computer.getId() + " - " + computer.getName());
+			}
+			System.out.println("Page " + (currentPage + 1) + " / " + nbPages);
+
+			// 4 cases : full list displayed, start of list displayed, end of list
+			// displayed, middle list displayed
+			// full display
+			boolean validChoice = false;
+			if (nbPages == 1) {
+				System.out.println("e to exit");
+				while (!validChoice) {
+					String choice = scan.nextLine();
+					if (!choice.equals("e")) {
+						System.out.println("Invalid choice");
+					} else {
+						validChoice = true;
+						return;
+					}
+				}
+			} else {
+				// middle list
+				if (currentPage != 0 && currentPage < (nbPages - 1)) {
+					System.out.println("n for next page - p for previous page - e to exit");
+					while (!validChoice) {
+						String choice = scan.nextLine();
+						switch (choice) {
+						case "n":
+							currentPage++;
+							validChoice = true;
+							break;
+						case "p":
+							currentPage--;
+							validChoice = true;
+							break;
+						case "e":
+							return;
+						default:
+							System.out.println("Invalid choice");
+							break;
+
+						}
+					}
+				}
+				// start list
+				else if (currentPage == 0) {
+					System.out.println("n for next page - e to exit");
+					while (!validChoice) {
+						String choice = scan.nextLine();
+						switch (choice) {
+						case "n":
+							currentPage++;
+							validChoice = true;
+							break;
+						case "e":
+							return;
+						default:
+							System.out.println("Invalid choice");
+							break;
+
+						}
+					}
+				}
+				// end list
+				else if (currentPage == nbPages - 1) {
+					System.out.println("p for previous page - e to exit");
+					while (!validChoice) {
+						String choice = scan.nextLine();
+						switch (choice) {
+						case "p":
+							currentPage--;
+							validChoice = true;
+							break;
+						case "e":
+							return;
+						default:
+							System.out.println("Invalid choice");
+							break;
+
+						}
+					}
+				}
+
+			}
+		}
 	}
-	
+
 	private void showComputer() {
 		System.out.println("Show computer");
+		while(true) {
+			System.out.println("Enter computer name (or nothing to exit) :");
+			String name = new String();
+			name = scan.nextLine();
+			if(name.isEmpty()) {
+				return;
+			}
+			List<Computer> foundComputers = dp.getComputersByName(name);
+			if(foundComputers.isEmpty()) {
+				System.out.println(name+" not found");
+			}else {
+				for(Computer computer : foundComputers) {
+					System.out.println(computer.toString());
+				}
+			}
+		}
 	}
+
+	
+	
+	private Map<ComputerField,String> createComputerParamters(){
+		return null;
+		
+	}
+	
+	private void addComputer() {
+		System.out.println("Add computer");
+	}
+	
 	
 	private void updateComputer() {
 		System.out.println("Update computer infos");
 	}
-	
+
 	private void deleteComputer() {
-		System.out.println("5 - Delete computer");
+		System.out.println("Delete computer");
 	}
-	
+
 }
