4cdf7d433372f69c506076a842f7f23af1ba4729
root <root@excilys-Precision-WorkStation-T3500>
2018-10-22 12:09:33 +0200

    test : added DatabaseAccessor test cases

--- JavaTrainingCDB/src/main/java/com/excilys/cdb/Main.java
+++ JavaTrainingCDB/src/main/java/com/excilys/cdb/Main.java
@@ -29,7 +29,5 @@ public class Main {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
 	}
 }
---
+++ JavaTrainingCDB/src/test/com/excilys/cdb/persistence/DatabaseAccessorTest.java
@@ -0,0 +1,64 @@
+package com.excilys.cdb.persistence;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Jonasz Leflour
+ * 
+ *
+ */
+public class DatabaseAccessorTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testGetDatabaseAccessor() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetupDatabase() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetAllComputers() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetAllCompanies() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetCompanybyId() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetComputerById() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetComputerByName() {
+		fail("Not yet implemented");
+	}
+
+	public void testCreateComputer() {
+		fail("Not yet implemented");
+	}
+
+	public void testDeleteComputerByName() {
+		fail("Not yet implemented");
+	}
+
+	public void testDeleteComputerById() {
+		fail("Not yet implemented");
+	}
+
+	public void testUpdateComputer() {
+		fail("Not yet implemented");
+	}
+
+}
