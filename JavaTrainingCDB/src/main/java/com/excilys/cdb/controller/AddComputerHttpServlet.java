4191ab2edb9fe609b7d886dc0cc3ddb8e0a8a3d9
JonaszLeflour <root@excilys-Precision-WorkStation-T3500>
2018-10-25 14:46:23 +0200

    controller : added addcomputer servlet

---
+++ JavaTrainingCDB/src/main/java/com/excilys/cdb/controller/AddComputerHttpServlet.java
@@ -0,0 +1,45 @@
+package com.excilys.cdb.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AddComputerHttpServlet
+ */
+@WebServlet("/addcomputer")
+public class AddComputerHttpServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddComputerHttpServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		RequestDispatcher dispatcher = getServletContext()
+                .getRequestDispatcher("/WEB-INF/views/addComputer.jsp");
+        dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
