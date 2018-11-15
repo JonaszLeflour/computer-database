060b87d59aaa1d393671485076b05ddf7415447e
JonaszLeflour <jonasz.leflour@ecole.ensicaen.fr>
2018-11-15 12:07:12 +0100

    refactor : added css

--- JavaTrainingCDB/src/main/java/com/excilys/cdb/config/ServletConfig.java
+++ JavaTrainingCDB/src/main/java/com/excilys/cdb/config/ServletConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
  *
  */
 @Configuration
+@EnableWebMvc
 @ComponentScan({ "com.excilys.cdb.controller","com.excilys.cdb.dto","com.excilys.cdb.config"})
 public class ServletConfig implements WebMvcConfigurer { 
 	/**
