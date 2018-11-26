b9b4c82efd31c29ba629b585b5553af50bc80ba1
JonaszLeflour <jonasz.leflour@ecole.ensicaen.fr>
2018-11-26 17:44:14 +0100

    refactor : changed architecture

---
+++ JavaTrainingCDB/JavaTrainingCDB-webapp/src/main/java/com/excilys/cdb/config/ServletConfig.java
@@ -0,0 +1,43 @@
+package com.excilys.cdb.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/**
+ * @author Jonasz
+ *
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan({ "com.excilys.cdb.controller","com.excilys.cdb.dto","com.excilys.cdb.config"})
+public class ServletConfig implements WebMvcConfigurer { 
+	/**
+	 * @return resolver
+	 */
+	@Bean
+	public ViewResolver resolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+	
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+	
+	@Override
+	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+        registry.addResourceHandler("/fonts/**").addResourceLocations("/fonts/");
+     }
+
+}
