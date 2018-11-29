6c223e3c84d63f126be7e7fa193fd404c8b63dcd
JonaszLeflour <jonasz.leflour@ecole.ensicaen.fr>
2018-11-29 15:17:09 +0100

    refactor : fixed launch issues

--- JavaTrainingCDB/JavaTrainingCDB-webapp/src/main/java/com/excilys/cdb/config/SecurityConfig.java
+++ JavaTrainingCDB/JavaTrainingCDB-webapp/src/main/java/com/excilys/cdb/config/SecurityConfig.java
@@ -1,22 +1,25 @@
 package com.excilys.cdb.config;
 
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.util.Properties;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import com.excilys.cdb.model.User;
+//import com.excilys.cdb.model.User;
 import com.excilys.cdb.persistence.UserDAO;
 import com.excilys.cdb.service.UserService;
 
@@ -24,12 +27,8 @@ import com.excilys.cdb.service.UserService;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	@Bean
-	public PasswordEncoder  encoder() {
-	    return new BCryptPasswordEncoder();
-	}
-	//@Autowired
-	//private UserDetailsService userDetailsService;
+	@Autowired
+	private UserDetailsService userDetailsService;
 
 	@Autowired
 	UserService userService;
@@ -37,22 +36,36 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	UserDAO userDAO;
 	
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	
 	
 	@Override
 	protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-		Properties prop = readProperties("config.properties");
-		for(User user :userDAO.getUsers()) {
+		//Properties prop = readProperties("config.properties");
+		auth.authenticationProvider(authenticationProvider(userDetailsService));
+		
+		/*for(User user :userDAO.getUsers()) {
 			auth.inMemoryAuthentication()
-			 .withUser(prop.getProperty(user.getName())).password(user.getPassword()).roles(user.getRole().getName());
-		}
+			 .withUser(user.getName()).password(user.getPassword()).roles(user.getRole().getName());
+		}*/
+	}
+	
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider(UserDetailsService userDetailsService) {
+	    DaoAuthenticationProvider authProvider
+	      = new DaoAuthenticationProvider();
+	    authProvider.setUserDetailsService(userDetailsService);
+	    authProvider.setPasswordEncoder(passwordEncoder);
+	    return authProvider;
 	}
 	
 	
-	private static Properties readProperties(String configFileName) throws ClassNotFoundException, IOException {
+	/*private static Properties readProperties(String configFileName) throws ClassNotFoundException, IOException {
 		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
 		InputStream input = classLoader.getResourceAsStream(configFileName);
 		Properties prop = new Properties();
 		prop.load(input);
 		return prop;
-	}
+	}*/
 }
